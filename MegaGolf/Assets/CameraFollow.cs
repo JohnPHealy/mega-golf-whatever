9dcb508f7432fe127f82614ec0590631ee8ff47e
Naomi Kaufman <accounts@warducks.com>
2021-02-15 10:43:18 +0000

    Added Camera Follow Script
    Added the Camera Follow Script and put it in my level

---
+++ MegaGolf/Assets/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Target;
+
+    private float speed = 10f;
+    public Vector3 Offset;
+    private void FixedUpdate()
+    {
+        Vector3 desPos = Target.position + Offset;
+        Vector3 smoPos = Vector3.Lerp(transform.position, desPos, speed * Time.deltaTime);
+        transform.position = desPos;
+
+        transform.LookAt(Target);
+    }
+}
